5d542d5ca4e05c6c38a32620cf8123d32f32653d
DaveH <daveh@cliquesoft.org>
2025-10-03 21:28:38 -0400

    Initial Commit

---
+++ version.h
@@ -0,0 +1,9 @@
+/* version.h - version defines for thttpd and libhttpd */
+
+#ifndef _VERSION_H_
+#define _VERSION_H_
+
+#define SERVER_SOFTWARE "paged 2016.07.25.0"
+#define SERVER_ADDRESS "http://www.cliquesoft.org/#Projects"
+
+#endif /* _VERSION_H_ */
